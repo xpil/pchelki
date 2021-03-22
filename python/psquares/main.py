11e51e16d078bca2b900571376d9601133d9e057
Piotr L <github@xpil.eu>
2021-03-22 19:00:49 +0000

    initial version

--- python/psquares/main.py
+++ python/psquares/main.py
@@ -1,6 +1,4 @@
 k = 2
-for n in range(10, 1000):
-    if ((n**k)//10)**(1/k) == int(((n**k)//10)**(1/k)):
-        print('!', n, n**k)
-    if (int(str(n**k)[1:])**(1/k)) == int((int(str(n**k)[1:])**(1/k))):
-        print('!!', n, n**k)
+for n in range(10, 1000000000):
+    if ((n**k)//10)**(1/k) == int(((n**k)//10)**(1/k)) and ((n**k)//100)**(1/k) == int(((n**k)//100)**(1/k)):
+        print(n, n**k)
